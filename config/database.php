18c2a6b14e5b7d5e66c768b7d40385736ee2abfd
Rishbh Arora <vsryarora6879@gmail.com>
2025-04-05 04:50:15 +0530

    chore: use env for db connection

--- config/database.php
+++ config/database.php
@@ -1,6 +1,12 @@
 <?php
-    $con = new mysqli("localhost","root","","recipe");
+    $host = getenv("DB_HOST");
+    $user = getenv("DB_USER");
+    $password = getenv("DB_PASSWORD");
+    $database = getenv("DB_NAME");
+
+    $con = new mysqli($host, $user, $password, $database);
+
     if($con->connect_error) {
-        die("Connection Failed : " . $con->connect_error);
+        die("Connection Failed: " . $con->connect_error);
     }
 ?>
